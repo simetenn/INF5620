a870176ae2d4b2d90f48a3c97db4b39b76f322e9
simetenn <simetenn@gmail.com>
2013-10-11 20:47:21 +0200

    Fixed bugs, constant solution tests are now working and giving correct answers

--- oblig2/wave.py
+++ oblig2/wave.py
@@ -1,8 +1,8 @@
 import nose.tools as nt
 import pylab as p
+import sys
 
-
-def solver(b, Lx, dx, Ly, dy, T, dt, version="scalar"):
+def solver(I, V, q, f ,b, Lx, dx, Ly, dy, T, dt, version="scalar"):
     Nx = int(round(Lx/float(dx)))
     Ny = int(round(Ly/float(dy)))
     Nt = int(round(T/float(dt)))
@@ -14,30 +14,30 @@ def solver(b, Lx, dx, Ly, dy, T, dt, version="scalar"):
 
     #Create initial conditions stuff
     if version == 'vectorized':
-        init_vec(u, x, y, dt)
-        advance_vec(u, x, y, b, dx, dy, Nt, dt)
+        init_vec(u, I, V, x, y, dt)
+        advance_vec(u, q, f, x, y, b, dx, dy, Nt, dt)
     else:
-        init(u, Nx, Ny, x, y, dt)
-        advance(u, x, y, b, Nx, dx, Ny, dy, Nt ,dt)
+        init(u, I, V, Nx, Ny, x, y, dt)
+        advance(u, q, f, x, y, b, Nx, dx, Ny, dy, Nt ,dt)
                 
     return u
 
 
     
     
-def init(u, Nx, Ny, x, y, dt):
+def init(u, I, V, Nx, Ny, x, y, dt):
 
-    for i in xrange(1, Nx-1):
-        for j in xrange(1, Ny-1):
+    for i in xrange(0, Nx):
+        for j in xrange(0, Ny):
             u[i,j,0] = I(x[i],y[j])
 
-    for i in xrange(1, Nx-1):
-        for j in xrange(1, Ny-1):
+    for i in xrange(0, Nx):
+        for j in xrange(0, Ny):
             u[i,j,-1] = u[i,j,0] - dt*V(x[i],y[j])
+
             
 
-
-def advance(u, x, y, b, Nx, dx, Ny, dy, Nt, dt):
+def advance(u, q, f, x, y, b, Nx, dx, Ny, dy, Nt, dt):
     dt2 = dt**2
     dtdx2 = dt2/(2*dx**2)
     dtdy2 = dt2/(2*dy**2)
@@ -45,40 +45,42 @@ def advance(u, x, y, b, Nx, dx, Ny, dy, Nt, dt):
     for n in xrange(0,Nt-1):
         for i in xrange(1, Nx-1):
             for j in xrange(1, Ny-1):
-                scheme(u,i,j,n, i+1, i-1, j+1, j-1, x, y, dtdx2, dtdy2, dt2)
+                scheme(u, q, f,  i, j, n, i+1, i-1, j+1, j-1, x, y, dtdx2, dtdy2, dt2, dt, b)
+
+        p.set_printoptions(precision=2)
+        #print u[:,:,n+1]
+        #sys.exit(0)
 
         #Update boundaries
         for i in xrange(1,Nx-1):
             j = 0;
-            scheme(u,i,j,n, i+1, i-1, j+1, j+1, x, y, dtdx2, dtdy2, dt2)
+            scheme(u, q, f, i, j, n, i+1, i-1, j+1, j+1, x, y, dtdx2, dtdy2, dt2, dt, b)
             
             j = Ny-1;
-            scheme(u,i,j,n, i+1, i-1, j-1, j-1, x, y, dtdx2, dtdy2, dt2)
+            scheme(u, q, f, i, j, n, i+1, i-1, j-1, j-1, x, y, dtdx2, dtdy2, dt2, dt, b)
             
         for j in xrange(1,Ny-1):
             i = 0;
-            scheme(u,i,j,n, i+1, i+1, j+1, j-1, x, y, dtdx2, dtdy2, dt2)
+            scheme(u, q, f, i, j, n, i+1, i+1, j+1, j-1, x, y, dtdx2, dtdy2, dt2, dt, b)
             
             i = Nx-1;
-            scheme(u,i,j,n, i-1, i-1, j+1, j-1, x, y, dtdx2, dtdy2, dt2)
+            scheme(u,q, f, i, j, n, i-1, i-1, j+1, j-1, x, y, dtdx2, dtdy2, dt2, dt, b)
             
-            #return u 
-
-        #Update corners
+            #Update corners
         i = 0; j = 0
-        scheme(u,i,j,n, i+1, i+1, j+1, j+1, x, y, dtdx2, dtdy2, dt2)
+        scheme(u, q, f, i, j, n, i+1, i+1, j+1, j+1, x, y, dtdx2, dtdy2, dt2, dt, b)
 
         i = 0; j = Ny-1
-        scheme(u,i,j,n, i+1, i+1, j-1, j-1, x, y, dtdx2, dtdy2, dt2)
+        scheme(u, q, f, i, j, n, i+1, i+1, j-1, j-1, x, y, dtdx2, dtdy2, dt2, dt, b)
 
         i = Nx-1; j = 0
-        scheme(u,i,j,n, i-1, i-1, j+1, j+1, x, y, dtdx2, dtdy2, dt2)
+        scheme(u ,q, f, i, j, n, i-1, i-1, j+1, j+1, x, y, dtdx2, dtdy2, dt2, dt, b)
 
         i = Nx-1; j = Ny-1
-        scheme(u,i,j,n, i-1, i-1, j-1, j-1, x, y, dtdx2, dtdy2, dt2)
+        scheme(u, q, f, i, j, n, i-1, i-1, j-1, j-1, x, y, dtdx2, dtdy2, dt2, dt, b)
 
         
-def scheme(u, i, j, n, i2, i3, j2, j3, x ,y, dtdx2, dtdy2, dt2):
+def scheme(u, q, f, i, j, n, i2, i3, j2, j3, x ,y, dtdx2, dtdy2, dt2, dt, b):
     """
     Standard input is:
     i2 = i+1
@@ -95,63 +97,71 @@ def scheme(u, i, j, n, i2, i3, j2, j3, x ,y, dtdx2, dtdy2, dt2):
     dt2*f(i,j,n)
     u[i,j,n+1] /= 1 + b*dt
     """
-    u[i,j,n+1] = 2*u[i,j,n] - (1 - 0.5*b*dt)*u[i,j,n-1] + \
-    dtdx2*((q(x[i2],y[j]) + q(x[i],y[j]))*(u[i2,j,n] - u[i,j,n]) - (q(x[i],y[j]) + q(x[i3],y[j])*(u[i,j,n] -u[i3,j,n]))) + \
-    dtdy2*((q(x[i],y[j2]) + q(x[i],y[j]))*(u[i,j2,n] - u[i,j,n]) - (q(x[i],y[j]) + q(x[i],y[j3])*(u[i,j,n] -u[i,j3,n]))) + \
-    dt2*f(x[i],y[j],n)
-    u[i,j,n+1] /= 1 + 0.5*b*dt
-    
 
-def init_vec(u, x, y, dt):
+    #print 2*u[i,j,n]
+    #print (1 - 0.5*b*dt)*u[i,j,n-1] 
+    #print dtdx2*((q(x[i2],y[j]) + q(x[i],y[j]))*(u[i2,j,n] - u[i,j,n]) - (q(x[i],y[j]) + q(x[i3],y[j]))*(u[i,j,n] -u[i3,j,n]))
+    #print dtdy2*((q(x[i],y[j2]) + q(x[i],y[j]))*(u[i,j2,n] - u[i,j,n]) - (q(x[i],y[j]) + q(x[i],y[j3]))*(u[i,j,n] -u[i,j3,n]))
+    #print dt2*f(x[i],y[j],n)
+    
+    u[i,j,n+1] = 2*u[i,j,n] - (1 - 0.5*b*dt)*u[i,j,n-1] + \
+    dtdx2*((q(x[i2],y[j]) + q(x[i],y[j]))*(u[i2,j,n] - u[i,j,n]) - (q(x[i],y[j]) + q(x[i3],y[j]))*(u[i,j,n] -u[i3,j,n])) + \
+    dtdy2*((q(x[i],y[j2]) + q(x[i],y[j]))*(u[i,j2,n] - u[i,j,n]) - (q(x[i],y[j]) + q(x[i],y[j3]))*(u[i,j,n] -u[i,j3,n])) + \
+    dt2*f(x[i],y[j],n)
+    
+    u[i,j,n+1] /= 1 + 0.5*b*dt
+    #sys.exit(0)
+
+def init_vec(u, I, V, x, y, dt):
     u[:,:,0] = I(x,y)
     u[:,:,-1] = u[:,:,0] - dt*V(x,y)
             
 
     
-def advance_vec(u, x, y, b, dx, dy, Nt, dt):
+def advance_vec(u, q, f, x, y, b, dx, dy, Nt, dt):
     dt2 = dt**2
     dtdx2 = dt2/(2*dx**2)
     dtdy2 = dt2/(2*dy**2)
     
     for n in xrange(0,Nt-1):
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b)
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt)
 
         #boundary conditions
         #j = 0;
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b,\
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt, \
                    jstart = 0, jstop = 1, j2start = 1, j2stop = 2, j3start = 1, j3stop = 2)
     
         #j = Ny-1
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b,\
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt, \
                    jstart = -1, jstop = None, j2start = -2, j2stop = -1, j3start = -2, j3stop = -1)
 
         #i = 0
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b,\
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt, \
                    istart = 0, istop = 1, i2start = 1, i2stop = 2, i3start = 1, i3stop = 2)
                    
         #i = Nx-1
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b,\
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt, \
                    istart = -1, istop = None, i2start = -2, i2stop = -1, i3start = -2, i3stop = -1)
         
         #Create corners
         #i = 0; j = 0
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b, \
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt, \
                    istart = 0, istop = 1, i2start = 1, i2stop = 2, i3start = 1, i3stop = 2,\
                    jstart = 0, jstop = 1, j2start = 1, j2stop = 2, j3start = 1, j3stop = 2)
                
         #i = 0; j = Ny-1
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b, \
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt, \
                    istart = 0, istop = 1, i2start = 1, i2stop = 2, i3start = 1, i3stop = 2,\
                    jstart = -1, jstop = None, j2start = -2, j2stop = -1, j3start = -2, j3stop = -1)
                        
         #i = Nx-1; j = 0
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b, \
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt, \
                    istart = -1, istop = None, i2start = -2, i2stop = -1, i3start = -2, i3stop = -1,\
                    jstart = 0, jstop = 1, j2start = 1, j2stop = 2, j3start = 1, j3stop = 2)
                        
                
         #i = Nx-1; j = Ny-1
-        scheme_vec(u, n, x, y, dtdx2, dtdy2, dt2, b, \
+        scheme_vec(u, n, q, f, x, y, dtdx2, dtdy2, dt2, b, dt, \
                    istart = -1, istop = None, i2start = -2, i2stop = -1, i3start = -2, i3stop = -1,\
                    jstart = -1, jstop = None, j2start = -2, j2stop = -1, j3start = -2, j3stop = -1)
 
@@ -160,7 +170,7 @@ def advance_vec(u, x, y, b, dx, dy, Nt, dt):
 
                    
                    
-def scheme_vec(u, n, x ,y, dtdx2, dtdy2, dt2, b, \
+def scheme_vec(u, n, q, f, x ,y, dtdx2, dtdy2, dt2, b, dt, \
                istart = 1, istop = -1, i2start = 2, i2stop = None, i3start = 0, i3stop = -2,\
                jstart = 1, jstop = -1, j2start = 2, j2stop = None, j3start = 0, j3stop = -2):
 
@@ -177,21 +187,20 @@ def scheme_vec(u, n, x ,y, dtdx2, dtdy2, dt2, b, \
     - (q(x[istart:istop],y[jstart:jstop]) + q(x[istart:istop],y[j3start:j3stop])\
            *(u[istart:istop,jstart:jstop,n] -u[istart:istop,j3start:j3stop,n]))) + \
         dt2*f(x[istart:istop],y[jstart:jstop], dt*n)).shape
-        """
-
+    """
     u[istart:istop,jstart:jstop,n+1] = \
-          2*u[istart:istop,jstart:jstop,n] - (1 - 0.5*b*dt)*u[istart:istop,jstart:jstop,n-1] + \
-    dtdx2*((q(x[i2start:i2stop],y[jstart:jstop]) + q(x[istart:istop],y[jstart:jstop]))\
-           *(u[i2start:i2stop,jstart:jstop,n] - u[istart:istop,jstart:jstop,n]) \
-           - (q(x[istart:istop],y[jstart:jstop]) + q(x[i3start:i3stop],y[jstart:jstop])\
-              *(u[istart:istop,jstart:jstop,n] - u[i3start:i3stop,jstart:jstop,n]))) + \
+        2*u[istart:istop,jstart:jstop,n] - (1 - 0.5*b*dt)*u[istart:istop,jstart:jstop,n-1] + \
+        dtdx2*((q(x[i2start:i2stop],y[jstart:jstop]) + q(x[istart:istop],y[jstart:jstop]))\
+               *(u[i2start:i2stop,jstart:jstop,n] - u[istart:istop,jstart:jstop,n]) \
+               - (q(x[istart:istop],y[jstart:jstop]) + q(x[i3start:i3stop],y[jstart:jstop])\
+                  *(u[istart:istop,jstart:jstop,n] - u[i3start:i3stop,jstart:jstop,n]))) + \
     dtdy2*((q(x[istart:istop],y[j2start:j2stop]) + q(x[istart:istop],y[jstart:jstop]))\
            *(u[istart:istop,j2start:j2stop,n] - u[istart:istop,jstart:jstop,n]) \
     - (q(x[istart:istop],y[jstart:jstop]) + q(x[istart:istop],y[j3start:j3stop])\
-           *(u[istart:istop,jstart:jstop,n] -u[istart:istop,j3start:j3stop,n]))) + \
+        *(u[istart:istop,jstart:jstop,n] -u[istart:istop,j3start:j3stop,n]))) + \
         dt2*f(x[istart:istop],y[jstart:jstop], dt*n)
         
-    u[istart:istop,jstart:jstop,n+1] /= 1 + 0.5*b*dt
+    u[istart:istop,jstart:jstop,n+1] /= (1 + 0.5*b*dt)
     
             
     """
@@ -200,37 +209,97 @@ def scheme_vec(u, n, x ,y, dtdx2, dtdy2, dt2, b, \
     dtdy2*((q(x[i:j],y[i2:j2]) + q(x[i:j],y[i:j]))*(u[i:j,i2:j2,n] - u[i:j,i:j,n]) - (q(x[i:j],y[i:j]) + q(x[i:j],y[i3:j3])*(u[i:j,i:j,n] -u[i:j,i3:j3,n]))) + \
     dt2*f(x[i:j],y[i:j],n)
     u[i:j,i:j,n+1] /= 1 + 0.5*b*dt
-"""    
+   """    
 
     
+def test_constant_solution():
+    b = 0
+    Lx = 1.
+    Ly = 1.
+
+    dx = 0.1
+    dy = 0.1
+    dt = 0.1
+    T = 1.
+
+    Nx = int(round(Lx/float(dx)))
+    Ny = int(round(Ly/float(dy)))
+    Nt = int(round(T/float(dt)))
+
+    x = p.linspace(0, Lx, Nx)
+    y = p.linspace(0, Ly, Ny)
+    t = p.linspace(0, T, Nt)
     
-def V(x,y):
-    return 7
+    def V(x, y):
+        return 0
 
-def I(x, y):
-    return 3
+    def I(x, y):
+        return 4
 
-def q(x, y):
-    return 15000
+    def q(x, y):
+        return 5
 
-def f(x, y, n):
-    return 0
+    def f(x, y, n):
+        return 0
+
+    
+    u = solver(I, V, q, f, b, Lx, dx, Ly, dy, T, dt, version="scalar")
+
+    def exact_solution(x, y, t):
+         return 4
+    
+    u_e = exact_solution(x, y, t)
+    difference = abs(u_e - u).max()
+    print "Largest difference: ", difference
+    nt.assert_almost_equal(difference, 0, places=11)
 
 
 
-def testConstant():
-    b = 0.1
+def test_constant_solution_vec():
+    b = 0
     Lx = 10.
     Ly = 10.
 
     dx = 0.1
     dy = 0.1
-    dt = 0.1
-    T = 5.
+    dt = 0.01
+    T = 1.
+
+    Nx = int(round(Lx/float(dx)))
+    Ny = int(round(Ly/float(dy)))
+    Nt = int(round(T/float(dt)))
+
+    x = p.linspace(0, Lx, Nx)
+    y = p.linspace(0, Ly, Ny)
+    t = p.linspace(0, T, Nt)
     
-    u = solver(b, Lx, dx, Ly, dy, T, dt, version="vectorize")
+    def V(x, y):
+        return 0
+
+    def I(x, y):
+        return 10
+
+    def q(x, y):
+        return 0
+
+    def f(x, y, n):
+        return 0
+
     
+    u = solver(I, V, q, f, b, Lx, dx, Ly, dy, T, dt, version="vectorized")
+
+    def exact_solution(x, y, t):
+         return 10
+    
+    u_e = exact_solution(x, y, t)
+    difference = abs(u_e - u).max()
+    print "Largest difference: ", difference
+    nt.assert_almost_equal(difference, 0, places=11)
 
 
+    
+    
 if __name__ == '__main__':
+    test_constant_solution_vec()
+    test_constant_solution()
     print "something"
